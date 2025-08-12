68ef6c8eee4755ed2ab4d7c055bcbef095ae897a
Bubileone <bojan.djurich@gmail.com>
2025-08-12 16:38:29 +0200

    HomeAreaCalculatorRedo - update

--- src/main/java/objects/HomeAreaCalculatorRedo.java
+++ src/main/java/objects/HomeAreaCalculatorRedo.java
@@ -1,22 +1,32 @@
 package objects;
 
+import java.util.Scanner;
+
 public class HomeAreaCalculatorRedo {
+
+    private Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args){
 
-//Instantiating Objects:
-//        1. Using first type of Constructor
-        Rectangle room1 = new Rectangle();
-        room1.setLength(50);
-        room1.setWidth(25);
-        double areaOfRoom1 = room1.calculateArea();
-
-//        2. Using second type of Constructor
-        Rectangle room2 = new Rectangle(30, 75);
-        double areaOfRoom2 = room2.calculateArea();
-
-        double totalArea = areaOfRoom1 + areaOfRoom2;
+        HomeAreaCalculatorRedo calculator = new HomeAreaCalculatorRedo();
+        Rectangle room1 = calculator.getRoom();
+        Rectangle room2 = calculator.getRoom();
+        double totalArea = calculator.calculateAreaOfRoom(room1, room2);
         System.out.println("Area of both rooms: " + totalArea);
 
     }
+
+    public Rectangle getRoom(){
+        System.out.println("Enter the length of the room?");
+        double length = scanner.nextDouble();
+        System.out.println("Enter the width of the room?");
+        double width = scanner.nextDouble();
+
+        return new Rectangle(length, width);
+    }
+
+    public double calculateAreaOfRoom(Rectangle room1, Rectangle room2){
+        return room1.calculateArea() + room2.calculateArea();
+    }
+
 }
